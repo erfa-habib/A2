31db782a5c3729566df2fee9a842459117d216a8
Jubile <erfahabib@Erfas-MacBook-Pro.local>
2016-06-06 02:09:29 -0400

    started on start page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,9 @@
-
+window.onload = function() {
+    
+    var c = document.getElementById("main");
+    window.ctx = c.getContext("2d"); // Dealing with a global context is easier
+    draw();
+    
+    animate();
+    
+} // window.onload
